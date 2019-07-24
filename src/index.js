69674752e7c6f455ae634c54dcec094704f7dc8d
Manuel <manuel.suarez@icr.ch>
2019-07-24 15:47:41 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Support for the best browser in the world /s
+import 'react-app-polyfill/stable'; 
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
